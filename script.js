6fdcc86b42eab5b206cedef12ba7b6370c49366d
Mohammed Fadi <fademohammed48@gmail.com>
2024-08-03 09:57:51 +0200

    v1.1
    Test the git

---
+++ script.js
@@ -0,0 +1,35 @@
+'use strict';
+      let gist = Math.trunc(Math.random() * 30 + 1);
+      let score = 30;
+      
+      document.querySelector('.chack').addEventListener('click', function () {
+          const number = Number(document.querySelector('.number').value);
+          const gisting = document.querySelector('.gisting');
+          let hiscore = 0;
+          console.log(number);
+          if (!number || number == 0) {
+              gisting.textContent = '🙍‍♂️ دا ما رقم لاكن';
+          } else if (number > gist) {
+              gisting.textContent = '📈 كترت المحلبية';
+              score--;
+          } else if (number < gist) {
+              gisting.textContent = '📉 كدا حبة يا حبة';
+              score--;
+          } else if (number == gist) {
+              // When You Win
+              gisting.textContent = '🎉 باااس قرض على كدا';
+              document.querySelector('body').style.backgroundColor = '#e8f5e9';
+              document.querySelector('.cog').textContent = gist;
+              if (score > Number(hiscore)) {
+                  hiscore = score;
+                  document.querySelector('.hiscore').textContent = hiscore;
+              }
+          }
+          document.querySelector('.score').textContent = score;
+      });
+      
+      document.querySelector('.again').addEventListener('click', function () {
+          gist = Math.trunc(Math.random() * 30 + 1);
+          score = 30;
+          document.querySelector('body').style.backgroundColor = '#eeeeee';
+      });
